c9370ac20a5fb7a5507406ebaf77f3fde4b792bd
George Lyon <130179520+grly0n@users.noreply.github.com>
2025-03-20 16:24:16 -0700

    Implement 05 Sum Al

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) !== 'number' || typeof(end) !== 'number') return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    if (start % 1 !== 0 || end % 1 != 0) return 'ERROR';
 
+    let sum = 0;
+    if (start < end) for(start; start <= end; start++) sum += start;
+    else for(end; end <= start; end++) sum += end;
+    return sum;
 };
 
 // Do not edit below this line
