729099d4d252418a6a1b5965014e09ae223552f6
George Lyon <130179520+grly0n@users.noreply.github.com>
2025-03-20 15:45:43 -0700

    Implement 02 Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) return 'ERROR';
+    output = '';
+    for (let i = 0; i < count; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
