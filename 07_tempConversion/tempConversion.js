e2aa04c289c038483d48181e6aca510d5162d144
George Lyon <130179520+grly0n@users.noreply.github.com>
2025-03-20 16:36:18 -0700

    Implement 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let celsius = (degrees - 32) * (5/9);
+  return Number(celsius.toFixed(Number.isInteger(celsius) ? 0 : 1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let fahrenheit = degrees * (9/5) + 32
+  return Number(fahrenheit.toFixed(Number.isInteger(fahrenheit) ? 0 : 1));
 };
 
 // Do not edit below this line
