64ea91a3cd7c39f3b43b796f52db52446ff308ff
George Lyon <130179520+grly0n@users.noreply.github.com>
2025-03-20 15:48:13 -0700

    Implement 03 Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let output = '';
+    for (let i = word.length-1; i >= 0; i--) {
+        output += word[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
